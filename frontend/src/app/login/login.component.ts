16d37ee20fde8948760e7b9c25f418a3ce94abd9
rinat <rinat.khafizov@actum.cz>
2018-07-03 19:05:16 +0200

    [FE] Configured root component, started preparing login component

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  title = 'Type your user name';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
